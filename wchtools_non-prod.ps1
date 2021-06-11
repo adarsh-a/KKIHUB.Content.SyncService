f671bf33d2fdf66d85c6edadd4d76e4257dfa0c5
ayazchummun <ayaz.chummun@hangarww.com>
2021-06-11 15:20:24 +0400

    [Sync] - push content command initial

--- wchtools_non-prod.ps1
+++ wchtools_non-prod.ps1
@@ -1,6 +1,4 @@
-npx -i -g --production --no-optional wchtools-cli
 npx wchtools init --url https://content-eu-1.content-cms.com/api/37dd7bf6-5628-4aac-8464-f4894ddfb8c4 --user adarsh.bhautoo@hangarww.com --password Ad1108bh_hangarMU
-# ::call npx wchtools push -a
-# ::call npx wchtools push -c -f --password Ad1108bh_hangarMU
-Read-Host "Wait for a key press to exit"
-	
+$artifactPath = "$((Get-Location).path)\Artifacts\"
+npx wchtools push -c -f --dir $artifactPath --password Ad1108bh_hangarMU
+# Read-Host "Wait for a key press to exit"
